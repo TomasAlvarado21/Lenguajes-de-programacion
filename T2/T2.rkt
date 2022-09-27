25283d216eafa94b14d3d689a3ce1d4efcaa7bcc
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-09-27 14:35:50 -0300

    test
    tests en test

--- T2/T2.rkt
+++ T2/T2.rkt
@@ -59,38 +59,6 @@ s-Cmd ::=
     [(list Cond1 '& Cond2) (and Cond1 Cond2)]
     [(list Cond1 'orb Cond2) (or Cond1 Cond2)]))
 
-(test (parse-Cond '(#t orb #f)) #t)
-(test (parse-Cond '(#f & #f)) #f)
-(test (parse-Cond '(= a 1)) (= 'a 1))
-(test (parse-Cond '(< b 3)) (< 'b 3))
-(test (parse-Cond '(> c 12)) (> 'c 12))
-
-(test (parse-Cmd '(CREATE nombre (a b c)
-                          (FROM s SELECT regs WHERE (= s 1))))
-      (CREATE 'nombre '(a b c)
-              (FROM 's 'SELECT 'regs 'WHERE (= 's 1))))
-
-
-(test (parse-Cmd '(FROM s SELECT regs WHERE (= s 1))) (FROM 's 'SELECT 'regs 'WHERE (= 's 1)))
-
-
-(test (parse-Cmd '(INSERT (3 2 1) blabla
-                          (FROM blabla SELECT regs WHERE (= blabla 1))))
-      (INSERT '(3 2 1) 'blabla
-              (FROM 'blabla 'SELECT 'regs 'WHERE (= 'blabla 1))))
-
-
-
-(test (parse-Cmd '(CREATE toki-toki-ti (mesa terremotos empanadas)
-                         (INSERT (3 5 4) toki-toki-ti
-                                 (INSERT (2 2 4) toki-toki-ti
-                                         (INSERT (1 2 3) toki-toki-ti
-                                                 (FROM toki-toki-ti SELECT regs WHERE (= empanadas 4)))))))
-      (CREATE 'toki-toki-ti '(mesa terremotos empanadas)
-               (INSERT '(3 5 4) 'toki-toki-ti
-                       (INSERT '(2 2 4) 'toki-toki-ti
-                               (INSERT '(1 2 3) 'toki-toki-ti
-                                       (FROM 'toki-toki-ti 'SELECT 'regs 'WHERE (= 'empanadas 4)))))))
              
 
 #| ==============================
