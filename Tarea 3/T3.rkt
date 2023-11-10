15ef8ac83d48c2323d2f86fe8f4f5ef516732140
tomas <tomas.alvaradopi@gmail.com>
2023-11-10 18:17:35 -0300

    asdf

--- Tarea 3/T3.rkt
+++ Tarea 3/T3.rkt
@@ -1,4 +1,5 @@
 #lang play
+(print-only-errors #t)
 
 
 #|
@@ -17,6 +18,9 @@
   ;; unary first-class functions
   (id x)
   (fun binder binderType body)
+  (tt)
+  (ff)
+  (ifc c t e)
   (app callee arg)
   ;; new constructors
   (bool b)
@@ -42,9 +46,24 @@
     [(list '-> l r) (arrowT (parse-type l) (parse-type r))]
     [_ (error 'parse-type "invalid type: ~a" t)]))
 
-(test (parse-type 'Number) (numT))
-(test (parse-type 'Boolean) (boolT))
-(test (parse-type '(-> Number Boolean)) (arrowT numT boolT))
+
+
+;; parse : s-expr -> Expr
+(define (parse s)
+  (match s
+    [(quote true) (tt)]
+    [(quote false) (ff)]
+    [n #:when (number? n) (num n)]
+    [x #:when (symbol? x) (id x)]
+    [(list '+ l r) (binop '+ (parse l) (parse r))]
+    [(list '- l r) (binop '- (parse l) (parse r))]
+    [(list '* l r) (binop '* (parse l) (parse r))]
+    [(list '<= l r) (binop '<= (parse l) (parse r))]
+    [(list 'if c t f) (ifc (parse c) (parse t) (parse f))]
+    [(list 'fun (list binder ': type) body) (fun binder (parse-type type) (parse body))]
+    [(list callee arg) (app (parse callee) (parse arg))]
+    [_ (error 'parse "invalid syntax: ~a" s)]))
+
 
 ;; ambiente de tipos
 ;; TypeEnv ::= ⋅ | <TypeEnv>, <id> : <Type>
@@ -65,117 +84,33 @@
   (match expr
     [(num n) (numT)]
     [(bool b) (boolT)]
-    [(binop op l r) (match (cons (infer-type l tenv) (infer-type r tenv))
-                      [(cons (numT) (numT)) (numT)]
-                      [(cons (boolT) (boolT)) (boolT)]
-                      [_ (error 'infer-type "invalid operand type for ~a" op)])]
+    [(tt) (boolT)]
+    [(ff) (boolT)]
+    [(binop op l r) (match op
+                      ['<= (match (cons (infer-type l tenv) (infer-type r tenv))
+                             [(cons (numT) (numT)) (boolT)]
+                             [_ (error 'infer-type "invalid operand type for ~a" op)])]
+                      [_ (match (cons (infer-type l tenv) (infer-type r tenv))
+                            [(cons (numT) (numT)) (numT)]
+                            [_ (error 'infer-type "invalid operand type for ~a" op)])])]
     [(id x) (tenv-lookup x tenv)]
+    ;; ifc retorna el tipo de t si c es true, el tipo de e si c es false
+    [(ifc c t e)  
+     (match (infer-type c tenv)
+       [(boolT) (match (cons (infer-type t tenv) (infer-type e tenv))
+                  [(cons (numT) (numT)) (numT)]
+                  [_ (error "infer-type: if branches type mismatch")])]
+       [_ (error "infer-type: if condition must be a boolean")])]
+                    
+
+
     [(fun binder binderType body) (arrowT binderType (infer-type body (extend-tenv binder binderType tenv)))]
-    [(app call arg) (match (infer-type call tenv)
-                        [(arrowT T1 T2) (match (cons T1 (infer-type arg tenv))
-                                          [(cons T1 T2) T2]
-                                          [_ (error 'infer-type "invalid argument type")])]
-                        [_ (error 'infer-type "function application to a non-function")])]
-    [(leq l r) (match (cons (infer-type l tenv) (infer-type r tenv))
-                 [(cons (numT) (numT)) (boolT)]
-                 [_ (error 'infer-type "invalid operand type for <=")])]
-    [(app (app (id 'if) c) th) (match (cons (infer-type c tenv) (infer-type th tenv))
-                                 [(cons (boolT) T) T]
-                                 [_ (error 'infer-type "invalid argument type for if")])]
-    ))
-(infer-type (app (fun 'x (numT) (id 'x)) (fun 'x (numT) (id 'x))) empty-tenv)
-;  (infer-type (num 1) empty-tenv)
-;  (infer-type (fun 'x (numT) (id 'x)) empty-tenv)
-;  ( infer-type (fun 'x (arrowT (numT) (numT)) (id 'x)) empty-tenv)
-;;( infer-type (binop '+ (num 1) (fun 'x (numT) (id 'x))) empty-tenv)
- ;;( infer-type (app (num 1) (num 2)) empty-tenv)
-
-
-#| END P1 |#
-
-#| BEGIN P2 PREAMBLE |#
-
-;; ambiente de sustitución diferida
-(deftype Env
-  (mtEnv)
-  (aEnv id val env))
-
-;; interface ADT (abstract data type) del ambiente
-(define empty-env (mtEnv))
-
-;; "Simplemente" asigna un nuevo identificador para aEnv
-;(define extend-env aEnv)
-;;
-;; es lo mismo que definir extend-env así:
-;; (concepto técnico 'eta expansion')
-
-;; parse : s-expr -> Expr
-(define (parse s)
-  (match s
-    [n #:when (number? n) (num n)]
-    [(list 'true) (bool #t)]
-    [(list 'false) (bool #f)]
-    [x #:when (symbol? x) (id x)]
-    [(list '+ l r) (binop '+ (parse l) (parse r))]
-    [(list '- l r) (binop '- (parse l) (parse r))]
-    [(list '* l r) (binop '* (parse l) (parse r))]
-    [(list '<= l r) (leq (parse l) (parse r))]
-    [(list 'if c th el) (app (app (id 'if) (parse c)) (parse th))]
-    [(list 'fun (list binder ': type) body) (fun binder (parse-type type) (parse body))]
-    [(list callee arg) (app (parse callee) (parse arg))]
-    [_ (error 'parse "invalid syntax: ~a" s)]))
-
-#| END P2 |#
-;; parse : s-expr -> Expr
-(define (parse s)
-  (match s
-    [n #:when (number? n) (num n)]
-    [x #:when (symbol? x) (id x)]
-    [(list '+ l r) (binop '+ (parse l) (parse r))]
-    [(list '- l r) (binop '- (parse l) (parse r))]
-    [(list '* l r) (binop '* (parse l) (parse r))]
-    [(list 'fun (list binder ': type) body) (fun binder (parse-type type) (parse body))]
-    [(list callee arg) (app (parse callee) (parse arg))]
-    [_ (error 'parse "invalid syntax: ~a" s)]))
-
-;; Implementación de ambientes de tipos
-;; (análoga a la de ambientes de valores)
-
-;; TypeEnv ::= ⋅ | <TypeEnv>, <id> : <Type>
-(deftype TypeEnv (mtTenv) (aTenv id type env))
-(define empty-tenv (mtTenv))
-(define extend-tenv aTenv)
-
-(define (tenv-lookup x env)
-  (match env
-    [(mtTenv) (error 'tenv-lookup "free identifier: ~a" id)]
-    [(aTenv id type rest) (if (symbol=? id x) type (tenv-lookup x rest))]
-    ))
-
-;; infer-type : Expr TypeEnv -> Type
-;; al darle un expr y un ambiente de tipos, devuelve el tipo del expr
-;; tenemos literales numericos, adicionales(si ambos operandos tienen tipo numT, entonces el resultado es numT),  identificadores, funciones, aplicaciones
-
-(define (infer-type expr tenv) 
-  (match expr
-    [(num n) (numT)]
-    [(binop op l r) (match (cons (infer-type l tenv) (infer-type r tenv))
-                      [(cons (numT) (numT)) (numT)]
-                      [_ (error 'infer-type "invalid operand type for ~a" op)])]
-    [(id x) (tenv-lookup x tenv)]
-    [(fun binder binderType body) (arrowT binderType (infer-type body (extend-tenv binder binderType tenv)))]
-    [(app call arg) (match (infer-type call tenv)
-                        [(arrowT T1 T2) (match (cons T1 (infer-type arg tenv))
-                                          [(cons T1 T2) T2]
-                                          [_ (error 'infer-type "invalid argument type")])]
+    [(app callee arg) (match (infer-type callee tenv)
+                        [(arrowT T1 T2) (match (infer-type arg tenv)
+                                      (if (eq? T1 (infer-type arg tenv)) T2 (error 'infer-type "function argument type mismatch")))]
                         [_ (error 'infer-type "function application to a non-function")])]
     ))
-(infer-type (app (fun 'x (numT) (id 'x)) (fun 'x (numT) (id 'x))) empty-tenv)
-;  (infer-type (num 1) empty-tenv)
-;  (infer-type (fun 'x (numT) (id 'x)) empty-tenv)
-;  ( infer-type (fun 'x (arrowT (numT) (numT)) (id 'x)) empty-tenv)
-;;( infer-type (binop '+ (num 1) (fun 'x (numT) (id 'x))) empty-tenv)
- ;;( infer-type (app (num 1) (num 2)) empty-tenv)
+
 
 
 #| END P1 |#
@@ -218,12 +153,22 @@
 
 #| BEGIN P2 |#
 
-;; final? : ...
-(define (final? e) '???)
+;; final? : Expr -> Boolean
+;; determina si un expr es final
+(define (final? e) 
+  (match e
+    [(num n) #t]
+    [(bool b) #t]
+    [(fun binder binderType body) #t]
+    [(tt) #t]
+    [(ff) #t]
+    [_ #f]))
+
+
 
 (deftype Kont
   (mt-k) ; empty kont
-  ;; ...
+  (binop-l-k op r env kont)
   )
 
 (define empty-kont (mt-k))
@@ -233,20 +178,60 @@
   (st expr env kont)
   )
 
-;; inject : ...
-(define (inject expr) '???)
+;; inject : Expr -> State
+;; inyecta un expr en un estado inicial
+(define (inject expr) 
+  (st expr empty-env empty-kont))
 
-;; step : ...
-(define (step c) '???)
+
+
+;; step : State -> State
+;;  recibe un estado de la maquina CEK, y produce uno nuevo. no es recursiva ni llama a eval
+(define (step c) 
+  (match c
+    [(st (num n) env kont) (match kont
+                             [(mt-k) (st (num n) env kont)]
+                             [(binop-l-k op r env kont) (st r env (binop-r-k op n kont))])]
+    [(st (binop op l r) env kont) (st l env (binop-l-k op r env kont))]
+    [(st (id x) env kont) (st (env-lookup x env) env kont)]
+    [(st (fun binder binderType body) env kont) (st (fun binder binderType body) env kont)]
+    [(st (tt) env kont) (st (tt) env kont)]
+    [(st (ff) env kont) (st (ff) env kont)]
+    [(st (ifc c t e) env kont) (st c env (ifc-k t e env kont))]
+    [(st (app callee arg) env kont) (st callee env (app-k arg env kont))]
+    [(st (leq l r) env kont) (st l env (leq-l-k r env kont))]
+    [(st (binop '+ l r) env kont) (st l env (binop-l-k '+ r env kont))]
+    [(st (binop '- l r) env kont) (st l env (binop-l-k '- r env kont))]
+    [(st (binop '* l r) env kont) (st l env (binop-l-k '* r env kont))]
+    [(st (binop '<= l r) env kont) (st l env (binop-l-k '<= r env kont))]
+
+    [(st (ifc (tt) t e) env kont) (st t env kont)]
+    [(st (ifc (ff) t e) env kont) (st e env kont)]
+    [(st (ifc c t e) env kont) (st c env (ifc-k t e env kont))]
+    [(st (app (fun binder binderType body) arg) env kont) (st body (extend-env binder arg env) kont)]
+    [(st (leq (num n) (num m)) env kont) (st (ifc (binop '<= (num n) (num m)) (tt) (ff)) env kont)]
+
+    [(st (app 
+    [(st (leq-l r env kont) env kont) (match kont
+                                          [(mt-k) (st r env kont)]
+                                          [(binop-l-k op r env kont) (st r env (binop-l-k op r env kont))])]
+    
+    
+    
+    ))
+
+
 
 ;; eval : Expr -> Expr
-(define (eval expr)
-  (define (eval-until-final state)
-    (def (st expr _ kont) state)
-    (if (and (final? expr) (mt-k? kont))
-        expr
-        (eval-until-final (step state))))
-  (eval-until-final (inject expr)))
+; (define (eval expr)
+;   (define (eval-until-final state)
+;     (def (st expr _ kont) state)
+;     (if (and (final? expr) (mt-k? kont))
+;         expr
+;         (eval-until-final (step state))))
+;   (eval-until-final (inject expr)))
 
-;; run : ...
-(define (run s-expr) '???)
+;; run : s-expr -> Expr
+;; corre el programa
+; (define (run s-expr) 
+;   (eval (parse s-expr)))
--- Tarea 3/test.rkt
+++ Tarea 3/test.rkt
@@ -10,3 +10,29 @@
 
 (parse-type ' (-> (-> Number Number) Number))
 
+(test (parse-type 'Number) (numT))
+(test (parse-type 'Boolean) (boolT))
+(test (parse-type '(-> Number Boolean)) (arrowT (numT) (boolT)))
+
+
+(test/exn (infer-type (app (fun 'x (numT) (id 'x)) (fun 'x (numT) (id 'x))) empty-tenv) "function argument type mismatch")
+(test (parse-type 'Boolean) (boolT))
+(test (parse ' ( if (<= 5 6) true false )) (ifc (binop '<= (num 5) (num 6)) (tt) (ff)))
+
+(test (infer-type ( ifc (binop '<= (num 5) (num 6)) (num 2) (num 3))empty-tenv) (numT))
+(test/exn (infer-type ( ifc (num 5) (num 2) (num 3)) empty-tenv) "infer-type: if condition must be a boolean")
+
+(test/exn (infer-type ( ifc (binop '<= (num 5) (num 6)) (num 2) (tt)) empty-tenv) "infer-type: if branches type mismatch")
+
+
+(test (infer-type (num 1) empty-tenv) (numT))
+(test (infer-type (fun 'x (numT) (id 'x)) empty-tenv) (arrowT (numT) (numT)))
+(test (infer-type (fun 'x (arrowT (numT) (numT)) (id 'x)) empty-tenv) (arrowT (arrowT (numT) (numT)) (arrowT (numT) (numT))))
+(test/exn (infer-type (binop '+ (num 1) (fun 'x (numT) (id 'x))) empty-tenv) "invalid operand type for +")
+(test/exn (infer-type (app (num 1) (num 2)) empty-tenv) "function application to a non-function")
+
+
+
+(final? (num 1))
+(final? (fun 'x (numT) (id 'x)))
+(final? (binop '+ (num 1) (num 2)))
